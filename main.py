e72eda92eb81db2ea6294b7a57cb8fd1b6f2dd6a
baby7305 <songhaibei7305@163.com>
2022-03-05 15:09:53 +0800

    引入自定义根节点

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ kivy.require('2.0.0')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
 class RandomNumber(App):
   def build(self):
-    return BoxLayout()
+    return MyRoot()
 
 randomApp = RandomNumber()
 randomApp.run()
