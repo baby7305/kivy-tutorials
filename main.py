151bc282020b8f2c52071d8e78f951c1937b493a
baby7305 <songhaibei7305@163.com>
2022-03-05 15:04:04 +0800

    引入界面设计文件

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import kivy
 kivy.require('2.0.0')
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 class RandomNumber(App):
   def build(self):
-    return Label(text="Random Number Generator")
+    return BoxLayout()
 
 randomApp = RandomNumber()
 randomApp.run()
